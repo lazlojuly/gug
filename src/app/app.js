9973c3919e3efbda9860ee10848fdca5302692f6
lazlojuly <lazlo.gy@gmail.com>
2016-11-16 23:16:30 +0000

    add pwa manifests

--- src/app/app.js
+++ src/app/app.js
@@ -13,3 +13,13 @@ ReactDom.render(
   <SimpleComponent message="React Demo" />,
   document.querySelector('.js-app')
 )
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('ws.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
